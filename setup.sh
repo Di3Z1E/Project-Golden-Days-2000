58cd06d60a2c15cd87f078734c930230f3d5597a
David Azani <davidazani@gmail.com>
2024-07-27 23:06:56 +0300

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,66 +1,56 @@
 #!/bin/bash
 
-# Login as super user
-sudo -i
-
 # Update and Upgrade the local server
-apt update && apt upgrade -y
-
-# Install git
-apt install git -y
+sudo apt update && sudo apt upgrade -y
 
 # Install open-ssh
-apt install openssh-server -y
+sudo apt install openssh-server -y
 
 # Uninstall all conflicting packages
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do apt-get remove -y $pkg; done
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove -y $pkg; done
 
 # Add Docker's official GPG key:
-apt-get update
-apt-get install -y ca-certificates curl
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
 
 # Install the Docker engine
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Set docker user
-groupadd docker
-usermod -aG docker $USER
+sudo groupadd docker
+sudo usermod -aG docker $USER
 newgrp docker
 
 # Install Ansible
-apt install -y software-properties-common
-add-apt-repository --yes --update ppa:ansible/ansible
-apt install -y ansible
+sudo apt install -y software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install -y ansible
 
 # Install Jenkins
-apt update
-apt install -y fontconfig openjdk-17-jre
+sudo apt update
+sudo apt install -y fontconfig openjdk-17-jre
 java -version
 
-wget -O /usr/share/keyrings/jenkins-keyring.asc \
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
-  https://pkg.jenkins.io/debian-stable binary/ | tee \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-apt-get update
-apt-get install -y jenkins
+sudo apt-get update
+sudo apt-get install -y jenkins
 
 # Download game servers
-docker pull lacledeslan/gamesvr-goldsource-cstrike
-docker pull lacledeslan/gamesvr-cssource
-docker pull sourceservers/left4dead2
-docker pull krustowski/samp-server-docker
-
-
-# Exit super user once done
-exit
+sudo docker pull lacledeslan/gamesvr-goldsource-cstrike
+sudo docker pull lacledeslan/gamesvr-cssource
+sudo docker pull sourceservers/left4dead2
+sudo docker pull krustowski/samp-server-docker
