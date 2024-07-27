dc0735a708cc521d88fc8911126c85c1c4f5ab83
David Azani <davidazani@gmail.com>
2024-07-27 23:16:00 +0300

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -9,9 +9,6 @@ fi
 # Update and Upgrade the local server
 apt update && apt upgrade -y
 
-# Install git
-apt install git -y
-
 # Install open-ssh
 apt install openssh-server -y
 
@@ -19,7 +16,6 @@ apt install openssh-server -y
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do apt-get remove -y $pkg; done
 
 # Add Docker's official GPG key:
-apt-get update
 apt-get install -y ca-certificates curl
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
@@ -30,13 +26,9 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
 
 # Install the Docker engine
 apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-# Set docker user
-usermod -aG docker $USER
 
 # Install Ansible
 apt install -y software-properties-common
@@ -44,7 +36,6 @@ add-apt-repository --yes --update ppa:ansible/ansible
 apt install -y ansible
 
 # Install Jenkins
-apt update
 apt install -y fontconfig openjdk-17-jre
 java -version
 
@@ -53,7 +44,6 @@ wget -O /usr/share/keyrings/jenkins-keyring.asc \
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-apt-get update
 apt-get install -y jenkins
 
 # Download game servers
