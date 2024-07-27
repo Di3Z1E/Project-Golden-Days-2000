f4434f8900de7f52498cb514744339e190b1e069
David Azani <davidazani@gmail.com>
2024-07-27 18:36:21 +0300

    Added openssh & git setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 # Login as super user
 sudo -i
@@ -5,19 +6,18 @@ sudo -i
 # Update and Upgrade the local server
 apt update && apt upgrade -y
 
-
 # Install git
 apt install git -y
 
 # Install open-ssh
 apt install openssh-server -y
 
-# uninstall all conflicting packages
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+# Uninstall all conflicting packages
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove -y $pkg; done
 
 # Add Docker's official GPG key:
 apt-get update
-apt-get install ca-certificates curl
+apt-get install -y ca-certificates curl
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
@@ -30,20 +30,17 @@ echo \
 apt-get update
 
 # Install the Docker engine
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Install Ansible
-apt install software-properties-common
+apt install -y software-properties-common
 add-apt-repository --yes --update ppa:ansible/ansible
-apt install ansible
+apt install -y ansible
 
 # Install Jenkins
 apt update
-apt install fontconfig openjdk-17-jre
+apt install -y fontconfig openjdk-17-jre
 java -version
-openjdk version "17.0.8" 2023-07-18
-OpenJDK Runtime Environment (build 17.0.8+7-Debian-1deb12u1)
-OpenJDK 64-Bit Server VM (build 17.0.8+7-Debian-1deb12u1, mixed mode, sharing)
 
 wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
@@ -51,4 +48,4 @@ echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 apt-get update
-apt-get install jenkins
+apt-get install -y jenkins
