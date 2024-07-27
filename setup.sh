45537b1966b1c21b0b34f20b510ab50294ed8965
David Azani <davidazani@gmail.com>
2024-07-27 23:09:21 +0300

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,56 +1,65 @@
 #!/bin/bash
 
+# Elevate the script to run as root if not already
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit
+fi
+
 # Update and Upgrade the local server
-sudo apt update && sudo apt upgrade -y
+apt update && apt upgrade -y
+
+# Install git
+apt install git -y
 
 # Install open-ssh
-sudo apt install openssh-server -y
+apt install openssh-server -y
 
 # Uninstall all conflicting packages
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove -y $pkg; done
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do apt-get remove -y $pkg; done
 
 # Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install -y ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+apt-get update
+apt-get install -y ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
 
 # Install the Docker engine
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Set docker user
-sudo groupadd docker
-sudo usermod -aG docker $USER
+groupadd docker
+usermod -aG docker $USER
 newgrp docker
 
 # Install Ansible
-sudo apt install -y software-properties-common
-sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install -y ansible
+apt install -y software-properties-common
+add-apt-repository --yes --update ppa:ansible/ansible
+apt install -y ansible
 
 # Install Jenkins
-sudo apt update
-sudo apt install -y fontconfig openjdk-17-jre
+apt update
+apt install -y fontconfig openjdk-17-jre
 java -version
 
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  https://pkg.jenkins.io/debian-stable binary/ | tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
-sudo apt-get install -y jenkins
+apt-get update
+apt-get install -y jenkins
 
 # Download game servers
-sudo docker pull lacledeslan/gamesvr-goldsource-cstrike
-sudo docker pull lacledeslan/gamesvr-cssource
-sudo docker pull sourceservers/left4dead2
-sudo docker pull krustowski/samp-server-docker
+docker pull lacledeslan/gamesvr-goldsource-cstrike
+docker pull lacledeslan/gamesvr-cssource
+docker pull sourceservers/left4dead2
+docker pull krustowski/samp-server-docker
