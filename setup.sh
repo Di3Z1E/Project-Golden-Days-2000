174fbef000fadbea705a442b13766247937b0117
David Azani <davidazani@gmail.com>
2024-08-03 16:01:01 +0300

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -14,26 +14,24 @@ fi
 apt update && apt upgrade -y
 
 # Uninstall all conflicting packages
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do apt-get remove -y $pkg; done
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do
+  apt-get remove -y $pkg
+done
 
 # Install Ansible
 apt install -y software-properties-common
 add-apt-repository --yes --update ppa:ansible/ansible
 apt install -y ansible
 
-# Install anisble items
-ansible-playbook src/ansible/00_setup.yml -vvv
+# Install ansible items
+ansible-playbook ${HOME}/Project-Golden-Days-2000/src/ansible/00_setup.yml -vvv || { echo "Ansible playbook failed"; exit 1; }
 
-# Convert dox to unix
-dos2unix /samp/samp-setup.sh
+# Convert dos to unix
+dos2unix ${HOME}/Project-Golden-Days-2000/src/samp/samp-setup.sh || { echo "Failed to convert line endings"; exit 1; }
 
 # Set folders
-mkdir ${HOME}/golden_days_2000
-mkdir ${HOME}/golden_days_2000/game_servers
-
-mkdir ${HOME}/golden_days_2000/game_servers/samp
-
-mkdir ${HOME}/golden_days_2000/game_servers/cs
+mkdir -p ${HOME}/golden_days_2000/game_servers/samp
+mkdir -p ${HOME}/golden_days_2000/game_servers/cs
 
 # Download game servers
 docker pull krustowski/samp-server-docker
@@ -41,4 +39,7 @@ docker pull cs16ds/server:latest
 docker pull left4devops/l4d
 
 # Start all servers
-docker compose -f /all-game-servers/docker-compose.yml up -d
+docker compose -f ${HOME}/Project-Golden-Days-2000/src/all-game-servers/docker-compose.yml up -d || { echo "Docker Compose failed"; exit 1; }
+
+# Run additional setup script
+bash ${HOME}/Project-Golden-Days-2000/src/samp/samp-setup.sh || { echo "samp-setup.sh failed"; exit 1; }
