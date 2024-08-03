8519c31dd4c51e3be74f15811041b70fd69392db
David Azani <davidazani@gmail.com>
2024-08-03 13:03:13 +0300

    Updated setup and dockefile

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Set Variables
+export USER=$(whoami)
+export HOME=$(eval echo ~$USER)
+
 # Elevate the script to run as root if not already
 if [ "$EUID" -ne 0 ]; then
   echo "Please run as root"
@@ -21,7 +25,5 @@ add-apt-repository --yes --update ppa:ansible/ansible
 apt install -y ansible
 
 # Download game servers
-docker pull lacledeslan/gamesvr-goldsource-cstrike
-docker pull lacledeslan/gamesvr-cssource
-docker pull sourceservers/left4dead2
 docker pull krustowski/samp-server-docker
+docker pull cs16ds/server:latest
